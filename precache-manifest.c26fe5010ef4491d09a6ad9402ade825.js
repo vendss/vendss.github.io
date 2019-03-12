7c65acfb6ee72b3a0d7048625ab9784d2c00db37
vendss <48304465+vendss@users.noreply.github.com>
2019-03-12 23:35:31 +0000

    Add files via upload

---
+++ precache-manifest.c26fe5010ef4491d09a6ad9402ade825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99576dbdd09665fe34a92c2994ffee8a",
+    "url": "/static/media/pencil-alt-solid.99576dbd.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0a63657dcc883701906d",
+    "url": "/static/js/main.0a63657d.chunk.js"
+  },
+  {
+    "revision": "89c2c76d6c9c9ecb3b2c",
+    "url": "/static/js/2.89c2c76d.chunk.js"
+  },
+  {
+    "revision": "0a63657dcc883701906d",
+    "url": "/static/css/main.0e16d8a8.chunk.css"
+  },
+  {
+    "revision": "07fad6f18781af9fac82887a0b513ac9",
+    "url": "/index.html"
+  }
+];
